53fb5dc5bccc97987be0983ccf11a3e75f9920d0
Zach Nusbaum <zachdnusbaum@gmail.com>
2017-12-25 21:43:25 -0700

    request calendar access

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -252,7 +252,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'],
-    hd: 'pincannarx.com'
+    hd: 'pincannarx.com', access_type: 'offline', scope: 'email,profile,calendar'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
